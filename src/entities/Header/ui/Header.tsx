0123a1d78d9857f55c94e700fcb33d62a1ed7920
korobasssss <an.korobeynikova@gmail.com>
2024-11-14 13:52:34 +0300

    mobile menu

--- src/entities/Header/ui/Header.tsx
+++ src/entities/Header/ui/Header.tsx
@@ -1,8 +1,8 @@
 import { IPaths } from '@/shared/interfaces'
 import styles from './styles.module.scss'
-import { LoginIcon, LogoIcon, LogoutIcon } from '@/shared/assets'
-import { FC } from 'react'
-import { ButtonIcon, Link } from 'ui-kit-cars/main'
+import { LoginIcon, LogoIcon, LogoutIcon, MenuIcon } from '@/shared/assets'
+import { FC, useMemo, useState } from 'react'
+import { ButtonIcon, DrawerPopup, Link } from 'ui-kit-cars/main'
 import { EPaths } from '@/shared/enums'
 
 interface IHeader {
@@ -14,30 +14,60 @@ export const Header: FC<IHeader> = (
         paths
     }
 ) => {
+
+    const [isMenuClick, setMenuClick] = useState(false)
+
+    const visiblePaths = useMemo(() => {
+        return paths.map((path, index) => {
+            if (path.url !== EPaths.MAIN && path.url !== EPaths.SIGN_IN && path.url !== EPaths.SIGN_UP) {
+                return (
+                        <Link 
+                            key={index}
+                            url={path.url}
+                        >
+                            {path.title}
+                        </Link>
+                    )
+                if (path.url === EPaths.USERS) {
+                    // todo если авторизован, то ссылка на пользователей
+                } else {
+                    
+                }
+            }
+        })
+    }, [paths])
+
     return (
         <header className={styles.SHeader}>
-            <h1>
-                <LogoIcon/>
-            </h1>
+            <ButtonIcon
+                alt='logo'
+            >
+                <Link 
+                    url={EPaths.MAIN}
+                >
+                    <LogoIcon/>
+                </Link>
+            </ButtonIcon>
             <nav className={styles.SNav}>
-                {
-                    paths.map(path => {
-                        if (path.url !== EPaths.MAIN && path.url !== EPaths.SIGN_IN && path.url !== EPaths.SIGN_UP) {
-                            if (path.url === EPaths.USERS) {
-                                // todo если авторизован, то ссылка на пользователей
-                            } else {
-                                return (
-                                    <Link 
-                                        url={path.url}
-                                    >
-                                        {path.title}
-                                    </Link>
-                                )
-                            }
-                        }
-                    })
-                }
+                {visiblePaths}
             </nav>
+            <nav className={styles.SNavIconMobile}>
+                <ButtonIcon
+                    alt='menu icon'
+                    onClick={() => setMenuClick(true)}
+                >
+                    <MenuIcon/>
+                </ButtonIcon>
+            </nav>
+            <DrawerPopup
+                title='Меню'
+                isOpen={isMenuClick}
+                setIsOpen={() => setMenuClick(false)}
+            >
+                <div className={styles.SNavMobile}>
+                    {visiblePaths}
+                </div>
+            </DrawerPopup>
             <div className={styles.SAuthWrapper}> {/* todo проверка авторизации */}
                 <span>
                     Тачковая А.В.
--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -2,5 +2,6 @@ export { ReactComponent as LoginIcon } from './Login.svg';
 export { ReactComponent as LogoIcon } from './Logo.svg';
 export { ReactComponent as LogoutIcon } from './Logout.svg';
 export { ReactComponent as EditIcon } from './Edit.svg';
+export { ReactComponent as MenuIcon } from './Menu.svg';
 export { default as TempIcon } from './temp.svg';
 export { default as UserImg } from './UserImg.svg';
